5e8fe0c7f86b7877358cb1b8688722838f1b0461
jianghuihui0224 <1679068166@qq.com>
2019-04-20 12:44:02 +0800

    登录页面静态完成

---
+++ src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="login_container">
+    <div class="box">
+      <img src="../assets/images/logo.png" alt="logo">
+
+      <el-form :model="form">
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-user-fill" placeholder="请输入您的用户名" v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-key" type="password" placeholder="请输入您的密码" v-model="form.region"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button>重置</el-button>
+          <el-button type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.login_container {
+  height: 100%;
+  background: linear-gradient(45deg, #fff, rgb(4, 138, 97));
+}
+.login_container .box {
+  width: 400px;
+  height: 250px;
+  box-shadow: 0 0 10px #efefef;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -60%);
+  background: linear-gradient(45deg, #eee, rgb(22, 119, 90));
+  padding: 0px 20px;
+}
+.login_container .box img {
+  display: block;
+  margin: 15px auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入全局css样式
+import './assets/css/global.css'
+
+// 引入图标文件
+import './assets/fonts/iconfont.css'
+
+// 引入element-ui组件库
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI, {size: 'small'})
+
 Vue.config.productionTip = false
 
+// 作用:入口文件的作用是引入依赖的文件
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
